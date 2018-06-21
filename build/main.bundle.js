39c3cd745c94e1c9160aa034d3630518f18ac56c
Bernadetta Wąsowska <bernadetta.wasowska@gmail.com>
2018-06-21 12:57:49 +0200

    first commit

---
+++ build/main.bundle.js
@@ -0,0 +1,33 @@
+"use strict";
+
+function Phone(brand, price, color) {
+    this.brand = brand;
+    this.price = price;
+    this.color = color;
+}
+
+Phone.prototype.printInfo = function () {
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+};
+
+Phone.prototype.makeACall = function (number) {
+    console.log("The phone brand is " + this.brand + " is calling the number " + number + ".");
+};
+
+Phone.prototype.pressButton = function (button) {
+    console.log("Beep");
+};
+
+var iPhone6S = new Phone("Apple", 2250, "silver");
+var samsungGalaxyS6 = new Phone("Samsung", 3200, "black");
+var onePlusOne = new Phone("One Plus", 5032, "rose");
+
+iPhone6S.printInfo();
+samsungGalaxyS6.printInfo();
+onePlusOne.printInfo();
+
+iPhone6S.makeACall(123456789);
+samsungGalaxyS6.makeACall(23948566);
+onePlusOne.makeACall(943504534);
+
+onePlusOne.pressButton(1);
